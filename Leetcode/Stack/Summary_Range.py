9dfb93c58063084fcdcb803085be6485304728aa
Shyamsundar S S <shyamsundar93424@gmail.com>
2026-08-11 17:01:22 +0530

    Summary range

---
+++ Leetcode/Stack/Summary_Range.py
@@ -0,0 +1,26 @@
+class Solution:
+    def summaryRanges(self, nums: List[int]) -> List[str]:
+        result=[]
+        res=[]
+        for i in range(len(nums)):
+            if not res:
+                res.append(nums[i])
+            elif nums[i]==res[-1]+1:
+                res.append(nums[i])
+            else:
+                if len(res)>1:
+                    result.append(str(res[0])+"->"+str(res[-1]))
+                else:
+                    result.append(str(res[0]))
+                res=[nums[i]]
+        if res:
+            if len(res)>1:
+                result.append(str(res[0])+"->"+str(res[-1]))
+            else:
+                result.append(str(res[0]))
+                
+            print(res)
+        return result
+                
+
+        
