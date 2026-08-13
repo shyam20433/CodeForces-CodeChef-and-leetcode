41038289acb55a595db71eacc52c56e4c3fedb93
Shyamsundar S S <shyamsundar93424@gmail.com>
2026-08-13 10:02:06 +0530

    Chef and dolls

---
+++ Codechef/arrays/Chef_and_Dolls.py
@@ -0,0 +1,11 @@
+# cook your dish here
+t=int(input())
+for _ in range(t):
+    n=int(input())
+    res=0
+    for __ in range(n):
+        a=int(input())
+        res^=a 
+    print(res)
+        
+        
